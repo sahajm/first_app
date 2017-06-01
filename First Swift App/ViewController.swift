3cf33518f59cc2853af853223c13a8d4e9cb6364
sahaj makharia <sahajmakharia@ResNet-61-196.resnet.ucsb.edu>
2017-06-01 14:43:09 -0700

    Adding numbers

--- First Swift App/ViewController.swift
+++ First Swift App/ViewController.swift
@@ -12,15 +12,13 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var theLabel: UILabel!
     
-    var tapCount = 0
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
     
     @IBAction func pressIt(_ sender: Any) {
-        self.theLabel.text = "Thanks Mom"
-    
-        tapCount = tapCount + 1
-        if tapCount >= 11 {
-            theLabel.text = "You are going at it, take it easy"
-        }
+        self.theLabel.text = "Answer is ...\(Int(text1.text!)! + Int(text2.text!)!)"
     }
     
     override func viewDidLoad() {
